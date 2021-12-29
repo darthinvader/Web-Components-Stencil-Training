9d63548a74c4cc6db0354aa7eba97ea3b4b3a8cc
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-29 16:30:55 +0200

    [feat] custom attribute

--- Tooltip.js
+++ Tooltip.js
@@ -2,13 +2,15 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = "Some Dummy Tooltip text";
   }
   connectedCallback() {
+    this._tooltipText = this.getAttribute("text") || this._tooltipText;
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
     tooltipIcon.addEventListener("mouseenter", () => {
       this._tooltipContainer = document.createElement("div");
-      this._tooltipContainer.textContent = "This is the tooltip text";
+      this._tooltipContainer.textContent = this._tooltipText;
       this.appendChild(this._tooltipContainer);
     });
     tooltipIcon.addEventListener("mouseleave", () => {
