c18aeec22cece78e618d931b8c1d623d3b88e617
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-30 16:53:13 +0200

    [feat] using props with attributes

--- stencil-components/src/components/side-drawer/side-drawer.tsx
+++ stencil-components/src/components/side-drawer/side-drawer.tsx
@@ -7,7 +7,7 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string;
-
+  @Prop({ reflect: true }) open: boolean;
   render() {
     return (
       <aside>
