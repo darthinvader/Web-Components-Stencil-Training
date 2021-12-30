4a47e6d990234262ca624e46728cfe71458285b9
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-30 17:01:17 +0200

    [feat] mutable pro

--- stencil-components/src/components/side-drawer/side-drawer.tsx
+++ stencil-components/src/components/side-drawer/side-drawer.tsx
@@ -7,12 +7,18 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string;
-  @Prop({ reflect: true }) open: boolean;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer = () => {
+    this.open = false;
+  };
+
   render() {
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer}>X</button>
         </header>
         <main>
           <slot />
