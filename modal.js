3223f4d3c2910be92facb10966ed80c74f652c2e
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-30 14:14:32 +0200

    [feat]getting access to slots

--- modal.js
+++ modal.js
@@ -45,7 +45,7 @@ class Modal extends HTMLElement {
           padding: 1rem;
         }
 
-        ::sloted(h1) {
+        ::slotted(h1) {
           font-size: 1.25rem;
         }
 
@@ -78,6 +78,10 @@ class Modal extends HTMLElement {
         </section>
       </div>
     `;
+    const slots = this.shadowRoot.querySelectorAll("slot");
+    slots[1].addEventListener("slotchange", (event) => {
+      console.dir(slots[1].assignedNodes());
+    });
   }
 
   // attributeChangedCallback(name, oldValue, newValue) {
