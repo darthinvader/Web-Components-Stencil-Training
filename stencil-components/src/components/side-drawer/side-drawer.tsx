2eaa3ff365a8641e32206d19076e24852f9650e1
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-30 16:06:51 +0200

    [feat] styling stencil component with sass

--- stencil-components/src/components/side-drawer/side-drawer.tsx
+++ stencil-components/src/components/side-drawer/side-drawer.tsx
@@ -2,13 +2,15 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'ycp-side-drawer',
+  styleUrl: './side-drawer.scss',
+  shadow: true,
 })
 export class SideDrawer {
   render() {
     return (
-      <div>
+      <aside>
         <h1>The Side Drawer</h1>
-      </div>
+      </aside>
     );
   }
 }
