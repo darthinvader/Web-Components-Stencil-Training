d3b9f23f3f6e680515c4a875b253172e4dbba22c
Yiannis Psaradakis <psaradakisj@gmail.com>
2021-12-31 12:35:12 +0200

    [feat] new StockPrice component render

--- stencil-components/src/components/stock-pirce/stock-price.tsx
+++ stencil-components/src/components/stock-pirce/stock-price.tsx
@@ -1,4 +1,18 @@
 import { Component } from '@stencil/core';
-
+// Q19JRWNE8HZ6OHDK
 @Component({ tag: 'ycp-stock-price', styleUrl: './stock-price.scss', shadow: true })
-export class StockPrice {}
+export class StockPrice {
+  render() {
+    return (
+      <div>
+        <form>
+          <input id="stock-symbol" />
+          <button type="submit">Fetch</button>
+        </form>
+        <div>
+          <p>Price:{0}</p>
+        </div>
+      </div>
+    );
+  }
+}
